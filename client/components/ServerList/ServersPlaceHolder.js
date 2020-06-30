9c62d5ab26399ffdfe346e10d5282748fcda813d
fleshascs <35168446+fleshascs@users.noreply.github.com>
2020-06-30 23:38:27 +0300

    fix: make server list skeleton smaller

--- client/components/ServerList/ServersPlaceHolder.js
+++ client/components/ServerList/ServersPlaceHolder.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const PlaceHolder = () => (
-	<div className="mb-3 px-2" style={{ height: '40px' }}>
-		<ContentLoader
-			height={140}
-			primaryColor={'#9494941a'}
-			secondaryColor={'#9494941a'}
-		>
-			<rect x="0" y="0" rx="5" ry="5" width="30" height="30" />
-			<rect x="60" y="0" rx="4" ry="4" width="270" height="13" />
-			<rect x="60" y="20" rx="3" ry="3" width="250" height="10" />
-			<rect x="364.4" y="0" rx="5" ry="5" width="30" height="30" />
-		</ContentLoader>
-	</div>
+  <div className='mb-3 px-2' style={{ height: '40px' }}>
+    <ContentLoader height={140} primaryColor={'#9494941a'} secondaryColor={'#9494941a'}>
+      <rect x='0' y='0' rx='5' ry='5' width='30' height='30' />
+      <rect x='60' y='0' rx='4' ry='4' width='270' height='13' />
+      <rect x='60' y='20' rx='3' ry='3' width='250' height='10' />
+      <rect x='364.4' y='0' rx='5' ry='5' width='30' height='30' />
+    </ContentLoader>
+  </div>
 );
 
 function createPlaceHolder(shoutsNum = 1) {
-	let placeHolders = [];
-	for (let j = 0; j < shoutsNum; j++) {
-		placeHolders.push(<PlaceHolder key={j} />);
-	}
-	return placeHolders;
+  let placeHolders = [];
+  for (let j = 0; j < shoutsNum; j++) {
+    placeHolders.push(<PlaceHolder key={j} />);
+  }
+  return placeHolders;
 }
 
-const ShoutsPlaceHolder = () => (
-	<div className="mt-3">{createPlaceHolder(11)}</div>
-);
+const ShoutsPlaceHolder = () => <div className='mt-3'>{createPlaceHolder(8)}</div>;
 export default ShoutsPlaceHolder;
