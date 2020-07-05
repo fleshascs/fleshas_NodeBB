fa126b7dc8f84d7dc13b0b04ad7c0bbdab11df82
fleshascs <35168446+fleshascs@users.noreply.github.com>
2020-07-05 16:52:38 +0300

    fix: promise allSettled polifill for edge

--- client/_core/polyfills.js
+++ client/_core/polyfills.js
@@ -4,6 +4,7 @@ import assign from 'core-js/library/fn/object/assign';
 //import arrayIncludes from 'core-js/library/fn/array/includes';
 //import arrayFind from 'core-js/library/fn/array/find';
 import 'core-js/modules/es7.array.includes.js';
+//import 'core-js/features/promise/all-settled';
 
 String.prototype.includes = includes;
 String.prototype.repeat = repeat;
@@ -11,9 +12,26 @@ Object.assign = assign;
 //Array.prototype.includes = arrayIncludes;
 //Array.prototype.find = arrayFind;
 
+if (!promise.allSettled) {
+  Promise.allSettled = (promises) =>
+    Promise.all(
+      promises.map((promise, i) =>
+        promise
+          .then((value) => ({
+            status: 'fulfilled',
+            value
+          }))
+          .catch((reason) => ({
+            status: 'rejected',
+            reason
+          }))
+      )
+    );
+}
+
 if (!HTMLElement.prototype.scrollTo) {
-	HTMLElement.prototype.scrollTo = function(left, top) {
-		this.scrollTop = top;
-		this.scrollLeft = left;
-	};
+  HTMLElement.prototype.scrollTo = function (left, top) {
+    this.scrollTop = top;
+    this.scrollLeft = left;
+  };
 }
