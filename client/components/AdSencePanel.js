0fa2a394d1008150bd9b101f0c7c0b6faecfbd01
fleshascs <35168446+fleshascs@users.noreply.github.com>
2020-08-18 00:06:13 +0300

    fix: hmm

--- client/components/AdSencePanel.js
+++ client/components/AdSencePanel.js
@@ -1,5 +1,27 @@
 import React, { useEffect } from 'react';
 
+// const AdSencePanel = ({ slotId }) => {
+//   useEffect(() => {
+//     try {
+//       (window.adsbygoogle = window.adsbygoogle || []).push({});
+//     } catch (error) {
+//       console.error('adSence error', error);
+//     }
+//   }, []);
+
+//   return (
+//     <div className='ad' style={{ textAlign: 'center' }}>
+//       <ins
+//         style={{ display: 'block' }}
+//         data-ad-client='ca-pub-8300648839719622'
+//         data-ad-slot={slotId}
+//         data-ad-format='auto'
+//         data-full-width-responsive='true'
+//       />
+//     </div>
+//   );
+// };
+
 const AdSencePanel = ({ slotId }) => {
   useEffect(() => {
     try {
@@ -10,15 +32,13 @@ const AdSencePanel = ({ slotId }) => {
   }, []);
 
   return (
-    <div className='ad' style={{ textAlign: 'center' }}>
-      <ins
-        style={{ display: 'block' }}
-        data-ad-client='ca-pub-8300648839719622'
-        data-ad-slot={slotId}
-        data-ad-format='auto'
-        data-full-width-responsive='true'
-      />
-    </div>
+    <ins
+      className='adsbygoogle'
+      style={{ display: 'inline-block' }}
+      data-ad-client='ca-pub-8300648839719622'
+      data-ad-slot={slotId}
+    />
   );
 };
+
 export default AdSencePanel;
