5dba2ef15b09618df9b256a2d5b72f872246184e
fleshascs <35168446+fleshascs@users.noreply.github.com>
2020-08-25 23:16:58 +0300

    fix: ads

--- client/components/AdSencePanel.js
+++ client/components/AdSencePanel.js
@@ -34,7 +34,7 @@ const AdSencePanel = ({ slotId }) => {
   return (
     <ins
       className='adsbygoogle mt-3'
-      style={{ display: 'inline-block', width: `100%`, maxHeight: `300px`, textAlign: 'center' }}
+      style={{ display: 'inline-block', width: `100%`, height: `300px`, textAlign: 'center' }}
       data-ad-client='ca-pub-8300648839719622'
       data-ad-slot={slotId}
     />
